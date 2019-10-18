601ddb09e9e4bb21317da78ad1353a13d8ca884d
Jason Dykast <jason@dykast.us>
2019-10-18 11:21:20 -0700

    Typo and dodgeball banner display in mobile.

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<link rel="icon" type="image/png" href="favicon-96x96.png" sizes="96x96">
     -->
 	
-	<title>NorCal Winter Camp 20202</title>
+	<title>NorCal Winter Camp 2020</title>
 	
 	<!-- CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
